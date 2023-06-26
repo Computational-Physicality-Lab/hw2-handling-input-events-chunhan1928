9962acce91b03f71382962bc3335df97e689b295
b07902059 <45457598+b07902059@users.noreply.github.com>
2023-06-26 20:47:04 +0800

    fix everything with state pattern

--- create-your-own/input.js
+++ create-your-own/input.js
@@ -1,103 +1,154 @@
-/*
-* all the code for homework 2 goes into this file.
-You will attach event handlers to the document, workspace, and targets defined in the html file
-to handle mouse, touch and possible other events.
-
-You will certainly need a large number of global variables to keep track of the current modes and states
-of the interaction.
-*/
-
-// global variables
-let workspace = document.querySelector("#workspace")
-let targets = document.querySelectorAll(".target")
-let canSelect = true
-let selectedTarget = null
-let draggedTarget = null
-let dblclickDrag = false
-let draggedTargetOriginalPosition = {
-    top: "0px",
-    left: "0px",
+class Context {
+  constructor() {
+    this.targets = document.querySelectorAll(".target");
+    this.workspace = document.getElementById("workspace");
+    // this.chosenTarget = null;
+    this.state = new IdleState(this);
+  }
+  changeState(state) {
+    this.state = state;
+    console.log(`change state: ${this.state.constructor.name}`);
+  }
 }
 
+class State {
+  constructor(context) {
+    this.context = context;
+  }
+}
 
-// function setup
-function selectTarget(event) {
-    event.stopPropagation()
-    // console.log("canSelect: ", canSelect)
-    if (!canSelect) {
-        canSelect = true
-        return
+class IdleState extends State {
+  constructor(context) {
+    super(context);
+    // functions that binds to this object
+    this.boundMouseDown = this.mouseDown.bind(this);
+    this.boundDblClick = this.dblClick.bind(this);
+    this.boundDeselect = this.deselect.bind(this);
+    // targets -- mouse down event listener
+    for (let target of this.context.targets) {
+      target.addEventListener("mousedown", this.boundMouseDown);
+      target.addEventListener("dblclick", this.boundDblClick);
     }
-    if (event.target.matches(".target")) {
-        if (selectedTarget)
-            selectedTarget.style.backgroundColor = 'red'
-        selectedTarget = event.target
-        selectedTarget.style.backgroundColor = 'blue'
+    // workspace -- click event listener
+    this.context.workspace.addEventListener("click", this.boundDeselect);
+  }
+  deselect(event) {
+    if (event.target != this.context.workspace) return;
+    console.log(`deselect`);
+    for (let target of this.context.targets) {
+      target.style.backgroundColor = "red";
     }
-    else if (event.target.matches("#workspace")) {
-        if (selectedTarget) {
-            selectedTarget.style.backgroundColor = 'red';
-            selectedTarget = null;
-        }
+  }
+  mouseDown(event) {
+    console.log(`mouse down`);
+    this.changeState(new MouseDownState(this.context, event.target));
+  }
+  dblClick(event) {
+    console.log("dblclick");
+    this.changeState(new DragState(this.context, event.target));
+  }
+  changeState(state) {
+    // remove event listener
+    for (let target of this.context.targets) {
+      target.removeEventListener("dblclick", this.boundDblClick);
+      target.removeEventListener("mousedown", this.boundMouseDown);
     }
+    this.context.workspace.removeEventListener("click", this.boundDeselect);
+    // change state
+    this.context.changeState(state);
+  }
 }
-
-function initDraggedTarget(event) {
-    draggedTarget = event.target
-    draggedTargetOriginalPosition.top = event.target.style.top
-    draggedTargetOriginalPosition.left = event.target.style.left
-}
-
-function clearDraggedTarget(event) {
-    draggedTarget = null
-    draggedTargetOriginalPosition.top = "0px"
-    draggedTargetOriginalPosition.left = "0px"
-}
-
-function drag(event) {
-    if (draggedTarget) {
-        canSelect = false
-        let { x, y } = draggedTarget.getBoundingClientRect()
-        draggedTarget.style.left = (x + event.movementX) + "px"
-        draggedTarget.style.top = (y + event.movementY) + "px"
+class MouseDownState extends State {
+  constructor(context, target) {
+    super(context);
+    this.target = target;
+    this.boundMouseUp = this.mouseUp.bind(this);
+    this.boundDrag = this.drag.bind(this);
+    this.target.addEventListener("mouseup", this.boundMouseUp);
+    this.context.workspace.addEventListener("mousemove", this.boundDrag);
+  }
+  mouseUp(event) {
+    console.log(`mouse up`);
+    for (let target of this.context.targets) {
+      target.style.backgroundColor = "red";
     }
+    event.target.style.backgroundColor = "blue";
+    // remove event listener
+    this.target.removeEventListener("mouseup", this.boundMouseUp);
+    this.context.workspace.removeEventListener("mousemove", this.boundDrag);
+    // change IdleState
+    this.context.changeState(new IdleState(this.context));
+  }
+  drag(event) {
+    console.log("start drag");
+    // remove event listener
+    this.target.removeEventListener("mouseup", this.boundMouseUp);
+    this.context.workspace.removeEventListener("mousemove", this.boundDrag);
+    // change to DragState
+    this.context.changeState(new DragState(this.context, event.target));
+  }
 }
 
-// target event listener
-targets.forEach((target) => {
-    target.addEventListener('click', selectTarget)
-    target.addEventListener('mousedown', initDraggedTarget)
-    target.addEventListener('dblclick', initDraggedTarget)
-})
-
-// workspace event listener
-workspace.addEventListener('click', selectTarget)
-workspace.addEventListener('mouseup', clearDraggedTarget)
-workspace.addEventListener('mousemove', drag)
-workspace.setAttribute("tabindex", -1); // enable getting the keyboard focus: https://stackoverflow.com/questions/18928116/javascript-keydown-event-listener-is-not-working
-workspace.focus(); // give it the focus to start: https://stackoverflow.com/questions/6754275/set-keyboard-focus-to-a-div/6809236
-workspace.addEventListener('keydown', (event) => {
-    if (event.key == 'Escape' && draggedTarget) {
-        draggedTarget.style.left = draggedTargetOriginalPosition.left
-        draggedTarget.style.top = draggedTargetOriginalPosition.top
-        clearDraggedTarget(event)
+class DragState extends State {
+  constructor(context, target) {
+    super(context);
+    // target
+    this.target = target;
+    const { x, y } = target.getBoundingClientRect();
+    this.originalPosition = { x, y };
+    // bind function and add event listener
+    this.boundDrag = this.drag.bind(this);
+    this.boundMouseUp = this.mouseUp.bind(this);
+    this.boundAbort = this.abort.bind(this);
+    this.context.workspace.addEventListener("mousemove", this.boundDrag);
+    this.context.workspace.addEventListener("mouseup", this.boundMouseUp);
+    this.context.workspace.addEventListener("keydown", this.boundAbort); // need to add "tabindex" property on workspace to make it work
+  }
+  drag(event) {
+    console.log("dragging");
+    let { x, y } = this.target.getBoundingClientRect();
+    this.target.style.left = x + event.movementX + "px";
+    this.target.style.top = y + event.movementY + "px";
+  }
+  mouseUp(event) {
+    console.log("mouseup: end dragging");
+    this.changeState(new EndState(context));
+  }
+  abort(event) {
+    if (event.key != "Escape") {
+      return;
     }
-})
-
-
-// debug: document event listener
-for (let i = 0; i < targets.length; i++) {
-    targets[i].id = `t${i}`
+    console.log("abort: return to original position");
+    this.target.style.left = this.originalPosition.x + "px";
+    this.target.style.top = this.originalPosition.y + "px";
+    this.changeState(new EndState(context));
+  }
+  changeState(state) {
+    // remove event listener
+    this.context.workspace.removeEventListener("mousemove", this.boundDrag);
+    this.context.workspace.removeEventListener("mouseup", this.boundMouseUp);
+    this.context.workspace.removeEventListener("keydown", this.boundabort);
+    // change to assigned state
+    this.context.changeState(state);
+  }
 }
 
-document.addEventListener(
-    'click',
-    (event) => { console.log(event) },
-    { capture: true }
-)
+class EndState extends State {
+  /*
+  The state to go when a complex event is finished.
+  It will buffer a click event, and go back to IdleState.
+  (because if the IdleState detect a click event, it will deselect)
+  */
+  constructor(context) {
+    super(context);
+    this.boundToIdle = this.toIdle.bind(this);
+    this.context.workspace.addEventListener("click", this.boundToIdle);
+  }
+  toIdle() {
+    this.context.workspace.removeEventListener("click", this.boundToIdle);
+    this.context.changeState(new IdleState(context));
+  }
+}
 
-document.addEventListener(
-    'keydown',
-    (event) => { console.log(event) },
-    { capture: true }
-)
+// a context object
+let context = new Context();
